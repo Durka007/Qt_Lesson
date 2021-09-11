28c9757cae5580af445f1a8f1685980aae28bba0
Durka007 <andrejdymovskih@gmail.com>
2021-09-11 18:29:54 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <QTextStream>
+using namespace Qt;
+
+int main()
+{
+    QTextStream out(stdout);
+
+    QString a = "love";
+    a.append(" chees");
+    a.prepend("I ");
+    out<< a<<endl;
+    out << "The a string has " << a.count() << " characters" <<endl;
+    out << a.toUpper() << endl;
+    out << a.toLower() << endl;
+    return 0;
+}
